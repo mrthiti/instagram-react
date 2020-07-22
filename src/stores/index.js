ed8b32a656d0fdbd87fd77cf3099b619bbf397aa
Thiti Yamsung <t.yamsung@gmail.com>
2020-07-23 00:19:30 +0700

    Add suggetion

--- src/stores/index.js
+++ src/stores/index.js
@@ -2,10 +2,12 @@ import { combineReducers } from 'redux'
 import { createStore } from 'redux'
 import presentation from './presentation/reducer'
 import post from './post/reducer'
+import suggetion from './suggetion/reducer'
 
 const rootReducer = combineReducers({
   presentation,
-  post
+  post,
+  suggetion
 })
 
 export default createStore(rootReducer)
